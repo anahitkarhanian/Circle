d91d7590d57217b12b7c728b751b6274777e8e47
anahitkarhanian <anahit.karhanyan777@gmail.com>
2021-04-26 17:58:11 +0400

    Circle

---
+++ src/com/company/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	int radius = 3;
+	int pi = 3;
+	int area, perimete;
+        area =  pi*radius*radius;
+        perimete = 2*pi*radius;
+        System.out.println(area);
+        System.out.println(perimete);
+    }
+}
